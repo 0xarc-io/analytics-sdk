1d4ca81dba7fc86a501fc84de3830a46977bf4e9
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2022-09-26 17:56:07 +0300

    chore: provide umd  build (#8)

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import path from 'path'
+
+module.exports = {
+  entry: {
+    bundle: './src/index.ts',
+    'bundle.min': './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.umd.min.js',
+    libraryTarget: 'umd',
+    library: 'ArcxAnalyticsSdk',
+    libraryExport: 'ArcxAnalyticsSdk',
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
+  },
+}
