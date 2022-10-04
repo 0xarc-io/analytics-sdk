f12803ebba1380380f879821ec1817c4a4384a1f
Kale Miller <km@arcx.money>
2022-10-04 21:45:38 +0000

    Added some simple docstrings to the analytics SDK

--- src/ArcxAnalyticsSdk.ts
+++ src/ArcxAnalyticsSdk.ts
@@ -14,6 +14,10 @@ export class ArcxAnalyticsSdk {
     }
   }
 
+  //////////////////////
+  // Internal Methods //
+  //////////////////////
+
   private trackPagesChanges() {
     document.body.addEventListener('click', () => {
       requestAnimationFrame(() => {
@@ -25,43 +29,6 @@ export class ArcxAnalyticsSdk {
         /* eslint-enable @typescript-eslint/no-explicit-any */
       })
     }, true)
-  }
-
-  static async init(apiKey: string, config?: SdkConfig, arcxUrl = PROD_URL_BACKEND): Promise<ArcxAnalyticsSdk> {
-    const sdkConfig = { ...DEFAULT_SDK_CONFIG, ...config }
-
-    const identityId = (sdkConfig?.cacheIdentity && localStorage.getItem(IDENTITY_KEY)) || await this.postAnalytics(arcxUrl, apiKey, '/identify')
-
-    sdkConfig?.cacheIdentity && localStorage.setItem(IDENTITY_KEY, identityId)
-
-    return new ArcxAnalyticsSdk(apiKey, identityId, arcxUrl, sdkConfig)
-  }
-
-  event(event: string, attributes?: Attributes): Promise<string> {
-    return ArcxAnalyticsSdk.postAnalytics(this.arcxUrl, this.apiKey, '/submit-event', {
-      identityId: this.identityId,
-      event,
-      attributes: { ...attributes },
-    })
-  }
-
-  page(attributes: { url: string }): Promise<string> {
-    return this.event(PAGE_EVENT, attributes)
-  }
-
-  connectWallet(attributes: { account: string, chain: string | number }): Promise<string> {
-    return this.event(CONNECT_EVENT, attributes)
-  }
-
-  transaction(transactionType: string, transactionHash?: string, attributes?: Attributes): Promise<string> {
-    const _attributes: Attributes = {
-      type: transactionType,
-      ...attributes,
-    }
-    if (transactionHash) {
-      _attributes.transaction_hash = transactionHash
-    }
-    return this.event(TRANSACTION_EVENT, _attributes)
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -76,5 +43,51 @@ export class ArcxAnalyticsSdk {
     })
     const body = await response.json()
     return cast(body, asString)
+  }
+
+  ////////////////////
+  // Public Methods //
+  ////////////////////
+
+  /** Initialises the Analytics SDK with desired configuration. */
+  static async init(apiKey: string, config?: SdkConfig, arcxUrl = PROD_URL_BACKEND): Promise<ArcxAnalyticsSdk> {
+    const sdkConfig = { ...DEFAULT_SDK_CONFIG, ...config }
+
+    const identityId = (sdkConfig?.cacheIdentity && localStorage.getItem(IDENTITY_KEY)) || await this.postAnalytics(arcxUrl, apiKey, '/identify')
+
+    sdkConfig?.cacheIdentity && localStorage.setItem(IDENTITY_KEY, identityId)
+
+    return new ArcxAnalyticsSdk(apiKey, identityId, arcxUrl, sdkConfig)
+  }
+
+  /** Generic event logging method. Allows arbitrary events to be logged. */
+  event(event: string, attributes?: Attributes): Promise<string> {
+    return ArcxAnalyticsSdk.postAnalytics(this.arcxUrl, this.apiKey, '/submit-event', {
+      identityId: this.identityId,
+      event,
+      attributes: { ...attributes },
+    })
+  }
+
+  /** Logs page visit events. Only use this method is `trackPages` is set to `false`. */
+  page(attributes: { url: string }): Promise<string> {
+    return this.event(PAGE_EVENT, attributes)
+  }
+
+  /** Logs a wallet connect event. */
+  connectWallet(attributes: { account: string, chain: string | number }): Promise<string> {
+    return this.event(CONNECT_EVENT, attributes)
+  }
+
+  /** Logs an on-chain transaction made by an account. */
+  transaction(transactionType: string, transactionHash?: string, attributes?: Attributes): Promise<string> {
+    const _attributes: Attributes = {
+      type: transactionType,
+      ...attributes,
+    }
+    if (transactionHash) {
+      _attributes.transaction_hash = transactionHash
+    }
+    return this.event(TRANSACTION_EVENT, _attributes)
   }
 }
