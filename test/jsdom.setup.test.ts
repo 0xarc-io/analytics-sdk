40ebc4882289aef690e5cbee93d0d992e80ba59b
Gabrel T <g@arcx.money>
2022-11-23 16:25:38 -0500

    test(jsdom.setup.test.ts): cleanup after tests

--- test/jsdom.setup.test.ts
+++ test/jsdom.setup.test.ts
@@ -6,9 +6,13 @@ export const TEST_UTM_MEDIUM = 'cpc'
 export const TEST_UTM_CAMPAIGN = 'ad-camp'
 export const TEST_JSDOM_URL = `https://example.com/?utm_source=${TEST_UTM_SOURCE}&utm_medium=${TEST_UTM_MEDIUM}&utm_campaign=${TEST_UTM_CAMPAIGN}`
 
+let cleanup: () => void
+
 before(() => {
-  globalJsdom('', {
+  cleanup = globalJsdom('', {
     url: TEST_JSDOM_URL,
     referrer: TEST_REFERRER,
   })
 })
+
+after(() => cleanup())
