91609bb4b0859f0af8275afb91b3c3f1d6564e42
Gabrel T <g@arcx.money>
2022-12-01 12:02:13 -0500

    test(arcxanalyticsprovider.test.tsx): pass all tests, make tests deterministic

--- test/setup.ts
+++ test/setup.ts
@@ -1,21 +1,19 @@
-import globalJsdom from 'global-jsdom'
 import chai from 'chai'
+import globalJsdom from 'global-jsdom'
 import sinonChai from 'sinon-chai'
 import { TEST_JSDOM_URL, TEST_REFERRER } from './fixture'
 
 chai.use(sinonChai)
 
-let cleanup: () => void
-
 exports.mochaHooks = {
-  beforeEach() {
-    cleanup = globalJsdom('', {
+  beforeAll() {
+    this.cleanup = globalJsdom(undefined, {
       url: TEST_JSDOM_URL,
       referrer: TEST_REFERRER,
     })
   },
 
-  afterEach() {
-    cleanup()
+  afterAll() {
+    this.cleanup()
   },
 }
