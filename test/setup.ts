5ddf43e8c0eff2505d898efbcadd9aeae095d89d
Gabrel T <g@arcx.money>
2022-12-02 16:04:45 -0500

    test: write new track pages tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,19 +1,4 @@
 import chai from 'chai'
-import globalJsdom from 'global-jsdom'
 import sinonChai from 'sinon-chai'
-import { TEST_JSDOM_URL, TEST_REFERRER } from './constants'
 
 chai.use(sinonChai)
-
-exports.mochaHooks = {
-  beforeAll() {
-    this.cleanup = globalJsdom(undefined, {
-      url: TEST_JSDOM_URL,
-      referrer: TEST_REFERRER,
-    })
-  },
-
-  afterAll() {
-    this.cleanup()
-  },
-}
