cf2be984d46daf89f384daaea1ffdd2729fce7f6
Yaraslau Stsetskevich <yaraslau.s@gmail.com>
2022-12-12 17:31:15 +0100

    test: test click with js-dom context

--- test/ArcxAnalyticsProvider.test.tsx
+++ test/ArcxAnalyticsProvider.test.tsx
@@ -12,6 +12,7 @@ import sinon from 'sinon'
 import * as postRequestModule from '../src/helpers/postRequest'
 import {
   ATTRIBUTION_EVENT,
+  CLICK_EVENT,
   DEFAULT_SDK_CONFIG,
   FIRST_PAGE_VISIT,
   PAGE_EVENT,
@@ -50,7 +51,14 @@ const TestProvider = ({
     <ArcxAnalyticxContext.Consumer>
       {(sdk) => (
         <div>
-          <div>{sdk?.identityId}</div>
+          <div>Identity: {sdk?.identityId}</div>
+          <div
+            id="id-for-click"
+            data-testid="track-click"
+            className="test-classname-1 test-classname-2"
+          >
+            Text to click
+          </div>
           <div>{children}</div>
         </div>
       )}
@@ -83,9 +91,8 @@ const ChildTest = () => {
 describe('(int) ArcxAnalyticxProvider', () => {
   let cleanup: () => void
   let postRequestStub: sinon.SinonStub
-  let screen: RenderResult
 
-  before(() => {
+  beforeEach(() => {
     cleanup = globalJsdom(undefined, {
       url: TEST_JSDOM_URL,
       referrer: TEST_REFERRER,
@@ -104,21 +111,16 @@ describe('(int) ArcxAnalyticxProvider', () => {
     // Stub _trackPagesChange because requestAnimationFrame throws during testing
     // To be fixed in https://github.com/arcxmoney/analytics-sdk/issues/23
     sinon.stub(ArcxAnalyticsSdk.prototype, '_trackPagesChange' as any)
-
-    screen = render(<ChildTest />, { wrapper: TestProvider })
-    expect(await screen.findByText(TEST_IDENTITY)).to.exist
-
-    postRequestStub.resetHistory()
   })
 
   afterEach(sinon.restore)
 
   describe('Initialization', () => {
     it('initializes the SDK', async () => {
-      render(<TestProvider />)
+      const screen = render(<TestProvider />)
 
       expect(postRequestStub.calledOnceWith(DEFAULT_SDK_CONFIG.url, TEST_API_KEY, '/identify'))
-      expect(await screen.findByText(TEST_IDENTITY)).to.exist
+      expect(await screen.findByText(`Identity: ${TEST_IDENTITY}`)).to.exist
     })
 
     it('fails to initialize sdk if api key was not provided', () => {
@@ -139,19 +141,16 @@ describe('(int) ArcxAnalyticxProvider', () => {
     })
 
     it('makes a FIRST_PAGE_VISIT call with the UTM parameters', async () => {
-      sessionStorage.clear()
-      localStorage.clear()
-
       expect(window.location.href).to.eq(TEST_JSDOM_URL)
 
-      render(
+      const screen = render(
         <TestProvider
           providerOverrides={{
             config: { trackUTM: true, trackReferrer: false, trackPages: false, trackClicks: false },
           }}
         />,
       )
-      expect(await screen.findByText(TEST_IDENTITY)).to.exist
+      expect(await screen.findByText(`Identity: ${TEST_IDENTITY}`)).to.exist
 
       expect(postRequestStub.calledTwice).to.be.true
       expect(
@@ -175,9 +174,73 @@ describe('(int) ArcxAnalyticxProvider', () => {
           }),
       ).to.be.true
     })
+
+    describe('#trackClicks', () => {
+      let screen: RenderResult
+
+      const getClickEventBody = (elementId: string, content: string) => {
+        return {
+          identityId: TEST_IDENTITY,
+          event: CLICK_EVENT,
+          attributes: {
+            elementId,
+            content,
+          },
+        }
+      }
+
+      beforeEach(async () => {
+        screen = render(
+          <TestProvider
+            providerOverrides={{
+              config: {
+                trackUTM: false,
+                trackReferrer: false,
+                trackPages: false,
+                trackClicks: true,
+              },
+            }}
+          />,
+        )
+        expect(await screen.findByText(`Identity: ${TEST_IDENTITY}`)).to.exist
+
+        postRequestStub.resetHistory()
+      })
+
+      it('makes track clicks on element with defined classname and id', async () => {
+        screen.getByTestId('track-click').click()
+
+        expect(postRequestStub.getCall(0)).to.be.calledWithExactly(
+          DEFAULT_SDK_CONFIG.url,
+          TEST_API_KEY,
+          '/submit-event',
+          getClickEventBody('div#id-for-click.test-classname-1.test-classname-2', 'Text to click'),
+        )
+      })
+
+      it('makes track clicks on element without defined classname and id', async () => {
+        screen.getByText(`Identity: ${TEST_IDENTITY}`).click()
+
+        expect(postRequestStub.getCall(0)).to.be.calledWithExactly(
+          DEFAULT_SDK_CONFIG.url,
+          TEST_API_KEY,
+          '/submit-event',
+          getClickEventBody('div', 'Identity: test-identity'),
+        )
+      })
+    })
   })
 
   describe('#useArcxAnalytics', () => {
+    let screen: RenderResult
+
+    beforeEach(async () => {
+      screen = render(<ChildTest />, { wrapper: TestProvider })
+      expect(await screen.findByText(`Identity: ${TEST_IDENTITY}`)).to.exist
+
+      postRequestStub.resetHistory()
+    })
+
     it('posts a custom event', async () => {
       screen.getByText('fire custom event').click()
 
