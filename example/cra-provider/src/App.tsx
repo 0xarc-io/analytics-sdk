7d009a476a4e5e9e83b727ea69bf89e435e8b578
Gabrel T <g@arcx.money>
2023-02-08 14:47:21 -0500

    test(cra-provider): update console view to show websocket-transmitted events

--- example/cra-provider/src/App.tsx
+++ example/cra-provider/src/App.tsx
@@ -92,12 +92,6 @@ function App() {
             <EthereumEventsButtons />
           </div>
           <ConsoleView capturedRequests={capturedRequests} />
-          <button
-            className="rounded-full bg-white px-4 py-2 mb-4 hover:bg-gray-100 font-bold text-black"
-            onClick={() => setCapturedRequests([])}
-          >
-            Clear console
-          </button>
         </div>
       </div>
     </div>
--- example/cra-provider/src/ConsoleView.tsx
+++ example/cra-provider/src/ConsoleView.tsx
@@ -1,8 +1,30 @@
 import { Attributes, useArcxAnalytics } from '@arcxmoney/analytics'
+import { useEffect, useState } from 'react'
+import { Socket } from 'socket.io-client'
+import { CLIENT_RENEG_LIMIT } from 'tls'
 import { CustomRequest } from './types'
 
 export const ConsoleView = ({ capturedRequests }: { capturedRequests: CustomRequest[] }) => {
+  const [events, setEvents] = useState<CustomRequest[]>(capturedRequests)
   const sdk = useArcxAnalytics()
+
+  useEffect(() => {
+    if (!sdk) return
+
+    const socket = sdk['socket'] as Socket
+
+    socket.onAnyOutgoing((event, ...args) => {
+      setEvents((events) => [
+        ...events,
+        {
+          method: 'WS',
+          url: socket.io['uri'],
+          event,
+          attributes: args[0],
+        },
+      ])
+    })
+  }, [sdk])
 
   const lineToString = (url: string, method: string, event?: string, attributes?: Attributes) => {
     if (event) {
@@ -16,24 +38,34 @@ export const ConsoleView = ({ capturedRequests }: { capturedRequests: CustomRequ
     }`
   }
 
+  console.log('🚀 ~ file: ConsoleView.tsx:9 ~ ConsoleView ~ events', events, events.length)
   return (
-    <div className="mt-8 h-[30rem] overflow-auto scroll-smooth w-full bg-slate-900 p-4 font-mono flex flex-col gap-4 text-yellow-500">
-      <div className="font-bold mb-4">
-        Requests are being sent to {process.env.REACT_APP_ARCX_API_URL ?? 'api.arcx.money'}...
+    <>
+      <div className="mt-8 h-[30rem] overflow-auto scroll-smooth w-full bg-slate-900 p-4 font-mono flex flex-col gap-4 text-yellow-500">
+        <div className="font-bold mb-4">
+          Requests are being sent to {process.env.REACT_APP_ARCX_API_URL ?? 'api.arcx.money'}...
+        </div>
+        <div>
+          {sdk && '> SDK initialized with ID: ' + sdk.identityId}
+          {events.length > 0 &&
+            events.map((line, i) => (
+              <div key={i}>
+                {line.url.includes('http') && (
+                  <>
+                    <br />
+                    {lineToString(line.url, line.method, line.event, line.attributes)}
+                  </>
+                )}
+              </div>
+            ))}
+        </div>
       </div>
-      <div>
-        {sdk && '> SDK initialized with ID: ' + sdk.identityId}
-        {capturedRequests.map((line, i) => (
-          <div key={i}>
-            {line.url.includes('http') && (
-              <>
-                <br />
-                {lineToString(line.url, line.method, line.event, line.attributes)}
-              </>
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
+      <button
+        className="rounded-full bg-white px-4 py-2 mb-4 hover:bg-gray-100 font-bold text-black"
+        onClick={() => setEvents([])}
+      >
+        Clear console
+      </button>
+    </>
   )
 }
