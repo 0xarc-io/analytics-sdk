d18a0080b7868aa4efd68e4d718e836717bae70e
Gabrel T <g@arcx.money>
2023-02-09 18:19:55 -0500

    test(postrequest.test.ts): ensure the sdk version is set

--- test/postRequest.test.ts
+++ test/postRequest.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import sinon from 'sinon'
+import { SDK_VERSION } from '../src/constants'
 import { postRequest } from '../src/helpers'
 import { TEST_API_KEY } from './constants'
 
@@ -25,12 +26,13 @@ describe('(unit) postRequest', () => {
     const res = await postRequest('https://example.com/', TEST_API_KEY, 'v1', data)
 
     expect(res).to.equal('test response')
+    expect(SDK_VERSION).to.not.be.empty
     expect(global.fetch).to.have.been.calledOnceWithExactly('https://example.com/v1', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
         'x-api-key': TEST_API_KEY,
-        'x-sdk-version': 'local',
+        'x-sdk-version': SDK_VERSION,
       },
       body: JSON.stringify(data),
     })
