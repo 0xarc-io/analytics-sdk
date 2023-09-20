bd325d5ddf96e3300c019bdea601b3c7ddbe2762
Gabrel T <g@arcx.money>
2023-09-20 14:55:04 -0400

    refactor: use identity id in the session id

--- src/utils/generateUniqueID.ts
+++ src/utils/generateUniqueID.ts
@@ -1,13 +1,9 @@
-let counter = 0
-
 /**
  * Generates a unique ID.
  */
-export function generateUniqueID() {
+export function generateUniqueID(identityId: string) {
   const timestamp = new Date().getTime()
   const randomPart = Math.random().toString(36).substring(2)
-  // Counter increments on every call to ensure uniqueness even if called rapidly in succession
-  counter += 1
 
-  return `${timestamp}-${randomPart}-${counter}`
+  return `${timestamp}-${identityId}-${randomPart}`
 }
