5212db2ad5f6791a14641532f8587c92f5e9567d
classicus-eth <classicushd@gmail.com>
2024-07-18 12:32:14 -0400

    feat: update constants with new api changes

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -8,10 +8,10 @@ export const DEFAULT_SDK_CONFIG: SdkConfig = {
   trackTransactions: true,
   trackSigning: true,
   trackClicks: true,
-  url: 'https://prod.analytics.api.arcx.money/v1',
+  url: 'https://prod.clickstream.api.0xarc.io/v1',
 }
 
-export const IDENTITY_KEY = 'identity'
-export const SESSION_STORAGE_ID_KEY = 'arcx-session-id'
-export const CURRENT_URL_KEY = 'arcx-analytics-current-url'
+export const IDENTITY_KEY = '0xArc-identity'
+export const SESSION_STORAGE_ID_KEY = '0xArc-session-id'
+export const CURRENT_URL_KEY = '0xArc-analytics-current-url'
 export const SDK_VERSION = 'local'
