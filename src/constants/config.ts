8f2188c955ae876df0ebd0a638e6c045ce4cca1a
classicus-eth <classicushd@gmail.com>
2024-07-22 12:26:17 -0400

    fix: revert url to previous version until certs a provisioned

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -8,7 +8,9 @@ export const DEFAULT_SDK_CONFIG: SdkConfig = {
   trackTransactions: true,
   trackSigning: true,
   trackClicks: true,
-  url: 'https://prod.clickstream.api.0xarc.io/v1',
+  // TODO: Add back in once GCP Certificates are fixed (potentially linked to CrowdStrike)
+  // url: 'https://prod.clickstream.api.0xarc.io/v1',
+  url: 'https://prod.analytics.api.arcx.money/v1',
 }
 
 export const IDENTITY_KEY = '0xArc-identity'
